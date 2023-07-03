808e2e99f8f75bbdc3237b5e895fa26fe310a37b
Naijyk <133905330+Naijyk@users.noreply.github.com>
2023-07-03 10:55:56 +0200

    Add files via upload

---
+++ testscript.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo -n "Enter your name: "
+read name
+echo "Your name is: $name."
+echo -n "Enter your working directory: "
+read dir
+echo "Your working directory is: $dir."
+echo "done"
+
